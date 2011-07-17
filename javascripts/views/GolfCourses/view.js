f7bd2741f226de41187f15902bdbbe79502d647f
paulcampbell <paulgcampbell@gmail.com>
2011-07-17 22:53:54 +0100

    view page added to

--- javascripts/views/GolfCourses/view.js
+++ javascripts/views/GolfCourses/view.js
@@ -16,16 +16,18 @@ App.Views.GolfCourses.View = Backbone.View.extend({
 		 	var html = $('#ViewCourse').tmpl(this.golfCourse.toJSON());
 		 	// $('#HoleList').html(html);
 		 	// var fullHtml = $('#ViewCourse').templ(this.golfCourse.toJSON());
+		 	 $('#header h1').html(this.golfCourse.get("name") );
 			 $('#app').html(html);
         } else {
             out = "<h3>No golf course found.</h3>";
+    		$(this.el).html(out);
+        	$('#app').html(this.el);
         }
 		
 		$('#back').attr('href', '#')
 		$('#back').show();
 		
-        $(this.el).html(out);
-        $('#app').html(this.el);
+  
     }
 });
 
