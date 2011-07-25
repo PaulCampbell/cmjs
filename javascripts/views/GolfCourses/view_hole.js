108043e145c193552ca0d4dab57f582b905fb764
Paul Campbell <paulgcampbell@gmail.com>
2011-07-25 16:26:20 +0100

    geo-lookup added to holes.view

--- javascripts/views/GolfCourses/view_hole.js
+++ javascripts/views/GolfCourses/view_hole.js
@@ -14,6 +14,20 @@ App.Views.Holes.View = Backbone.View.extend({
         if(this.hole) {
 		 	var html = $('#ViewHole').tmpl(this.hole.toJSON());
 			 $('#app').html(html);
+			  GetLocation();
+			 
+			  function GetLocation() {
+                    var win = function(position) {                          // Grab coordinates object from the Position object passed into success callback.
+                         var coords = position.coords;
+                         alert( coords.latitude + "," + coords.longitude )
+                    };
+                    var fail = function(e) {
+                         alert('Can\'t retrieve position.\nError: ' + e);
+                    };
+                    navigator.geolocation.getCurrentPosition(win, fail);
+               } 
+			 
+			 
         } else {
             out = "<h3>No hole found.</h3>";
     		$(this.el).html(out);
